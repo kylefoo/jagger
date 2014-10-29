2866e054928714e3bccb6ed593f9f7a8945ff6d7
Kyle Foo <kylefoo@users.noreply.github.com>
2014-10-29 10:28:02 -0700

    Update redis.rb
    added lines to refer to redis.yml for connection configuration, instead of using hiredis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,3 @@
-$redis = Redis.new(:driver => :hiredis)
+#$redis = Redis.new(:driver => :hiredis)
+REDIS_CONFIG = YAML::load_file(Rails.root.join('config', 'redis.yml'))
+$redis = Redis.new(:host => REDIS_CONFIG['host'], :port => REDIS_CONFIG['port'])
